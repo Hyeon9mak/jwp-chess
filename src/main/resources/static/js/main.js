5a678727225bbc4d574b133cd6ca700b379a99dc
hyeon9mak <jinha3507@gmail.com>
2021-04-24 16:35:46 +0900

    feat: add popup for create new game

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,7 +12,8 @@ const chessAPI = "/api/v1/chess";
 const chessURL = "/chess";
 
 function createRoom() {
-    let name = prompt("방 이름을 입력해주세요");
+    let name = prompt("방 이름을 입력해주세요.");
+    let password = prompt("플레이어 1의 비밀번호를 입력해주세요.");
     if (name !== "") {
         newGame(name);
     } else {
@@ -39,4 +40,21 @@ function newGame(name) {
         const errorMessage = response.responseText;
         location.href = chessURL + "/error-page?error=" + errorMessage;
     });
+}
+
+function showPopup() {
+    const popup = document.querySelector('#popup');
+    popup.classList.remove('hide');
+}
+
+function closePopup() {
+    const roomName = document.querySelector('#room-name');
+    const userId = document.querySelector('#user-id');
+    const userPassword = document.querySelector('#user-password');
+    roomName.value = "";
+    userId.value = "";
+    userPassword.value = "";
+
+    const popup = document.querySelector('#popup');
+    popup.classList.add('hide');
 }
